d5216959427bdf10d275b1c115fdf46d0ad93850
oscar <oscar.novillo@gmail.com>
2022-09-20 09:43:03 +0200

    asddasd

--- MOVILES/primeraapp/app/src/main/java/com/example/primeraapp/MainActivity.kt
+++ MOVILES/primeraapp/app/src/main/java/com/example/primeraapp/MainActivity.kt
@@ -2,9 +2,12 @@ package com.example.primeraapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import com.example.primeraapp.data.Data
 import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
@@ -17,6 +20,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val d = Data("hh")
+        d.user = "hh"
+
+
 
         tvName = this.findViewById(R.id.cajaTexto)
         btText = this.findViewById(R.id.button)
@@ -24,7 +31,14 @@ class MainActivity : AppCompatActivity() {
         ponerTexto()
 
         btText.setOnClickListener {
-            tvName.text = "Hola"
+            Log.w(":::TAG", "onCreate: ",)
+            if (tvName.text == "Hola mundo") {
+                Toast.makeText(this, "Hola ${tvName.text}", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "No has escrito nada", Toast.LENGTH_SHORT).show()
+            }
+
+            tvName.text = R.string.hello_world.toString()
         }
 
 
