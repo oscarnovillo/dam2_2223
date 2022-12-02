61ed4c8061d6a280ea13dac4dd3d43452fece3ad
oscar <oscar.novillo@gmail.com>
2022-12-02 09:49:05 +0100

    asdasdasd

--- PSP/multimoduloRest/servidor/src/main/java/ServletMail.java
+++ PSP/multimoduloRest/servidor/src/main/java/ServletMail.java
@@ -3,7 +3,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import utils.Utils;
+
 
 import java.io.IOException;
 
