d01caeb79b1fe1581227a8e0d132563db64c5d20
pepe <oscar.novillo@gmail.com>
2023-01-24 00:21:04 +0100

    ggg

--- MOVILES/firstCompose/app/src/main/java/com/example/fistcompose/MainActivity.kt
+++ MOVILES/firstCompose/app/src/main/java/com/example/fistcompose/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.twotone.Earbuds
@@ -20,6 +21,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
@@ -28,7 +30,9 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import coil.compose.AsyncImage
 import coil.compose.rememberImagePainter
+import coil.request.ImageRequest
 import coil.transform.CircleCropTransformation
 import com.example.fistcompose.ui.theme.FistComposeTheme
 import kotlinx.coroutines.launch
@@ -76,62 +80,77 @@ fun PrimerBox() {
             c.start()
             c
         }
-        Image(
-            painter = rememberImagePainter(
-                data = "https://placebear.com/200/300",
-                builder = {
-                    placeholder(circularProgressDrawable)
-                    transformations(CircleCropTransformation())
-                    crossfade(durationMillis = 2000)
-                }
-            ),
-            contentDescription = null,
-            modifier = Modifier.fillMaxSize(),
-            contentScale = ContentScale.Fit
+        AsyncImage(
+            // cargar imagen con ImageRequest
+            model = ImageRequest.Builder(LocalContext.current)
+                .data("https://loremflickr.com/320/240")
 
+                .build(),
 
-        )
+            contentDescription = "Translated description of what the image contains",
+            modifier = Modifier
+                .size(200.dp)
+                .clip(RoundedCornerShape(10.dp))
+                .clickable {
+                    Toast.makeText(context, "Hola", Toast.LENGTH_SHORT).show()
+                },
+            contentScale = ContentScale.Crop,
+
+            )
+
+//        Image(
+//            painter = rememberImagePainter(
+//                data = "https://placebear.com/200/300",
+//                builder = {
+//                    placeholder(circularProgressDrawable)
+//                    transformations(CircleCropTransformation())
+//                    crossfade(durationMillis = 2000)
+//                }
+//            ),
+//            contentDescription = null,
+//            modifier = Modifier.fillMaxSize(),
+//            contentScale = ContentScale.Fit
+//        )
         val lista =
             listOf(3, 1, 1, 1)
 
 
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(Color.Gray)
-        ) {
-
-
-            items(lista) { numero ->
-                val color = MaterialTheme.colors
-                var background by remember{ mutableStateOf(color.primary) }
-                Row(
-                    Modifier
-                        .clickable {
-                            background =
-                                if (background == color.secondary) color.primary else color.secondary
-                        }
-                        .background(background)) {
-                    Button(
-                        onClick = {
-
-                        },
-
-                        ) {
-                        Text(text = "Hola $numero")
-                    }
-                    if (background != color.secondary) {
-                        Text(
-                            text = numero.toString(),
-
-
-                            style = MaterialTheme.typography.h6,
-                        )
-                    }
-                }
-            }
-        }
-
+//        LazyColumn(
+//            modifier = Modifier
+//                .fillMaxSize()
+//                .background(Color.Gray)
+//        ) {
+//
+//
+//            items(lista) { numero ->
+//                val color = MaterialTheme.colors
+//                var background by remember{ mutableStateOf(color.primary) }
+//                Row(
+//                    Modifier
+//                        .clickable {
+//                            background =
+//                                if (background == color.secondary) color.primary else color.secondary
+//                        }
+//                        .background(background)) {
+//                    Button(
+//                        onClick = {
+//
+//                        },
+//
+//                        ) {
+//                        Text(text = "Hola $numero")
+//                    }
+//                    if (background != color.secondary) {
+//                        Text(
+//                            text = numero.toString(),
+//
+//
+//                            style = MaterialTheme.typography.h6,
+//                        )
+//                    }
+//                }
+//            }
+//        }
 
 
         Greeting(
@@ -147,45 +166,45 @@ fun PrimerBox() {
             name = stringResource(R.string.ivan),
             modifier = Modifier.align(Alignment.BottomEnd)
         )
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceEvenly
-        ) {
-
-            val scope = rememberCoroutineScope()
-
-            Icon(
-
-                imageVector = Icons.TwoTone.Earbuds,
-                contentDescription = null,
-                modifier = Modifier
-                    .size(200.dp)
-                    .clickable {
-
-                        context.startActivity(Intent(context, MainActivity2::class.java))
-
-                    },
-                tint = MaterialTheme.colors.primary,
-
-                )
-            Icon(
-                painter = painterResource(id = R.drawable.ic_baseline_ac_unit_24),
-                contentDescription = null,
-                modifier = Modifier
-                    .size(200.dp)
-                    .clickable {
-                        scope.launch {
-                            snackbarHostState.showSnackbar("Hello")
-                        }
-                        Toast
-                            .makeText(context, "TOAST", Toast.LENGTH_SHORT)
-                            .show()
-
-                    },
-
-                tint = MaterialTheme.colors.primary,
-            )
-        }
+//        Row(
+//            modifier = Modifier.fillMaxWidth(),
+//            horizontalArrangement = Arrangement.SpaceEvenly
+//        ) {
+//
+//            val scope = rememberCoroutineScope()
+//
+//            Icon(
+//
+//                imageVector = Icons.TwoTone.Earbuds,
+//                contentDescription = null,
+//                modifier = Modifier
+//                    .size(200.dp)
+//                    .clickable {
+//
+//                        context.startActivity(Intent(context, MainActivity2::class.java))
+//
+//                    },
+//                tint = MaterialTheme.colors.primary,
+//
+//                )
+//            Icon(
+//                painter = painterResource(id = R.drawable.ic_baseline_ac_unit_24),
+//                contentDescription = null,
+//                modifier = Modifier
+//                    .size(200.dp)
+//                    .clickable {
+//                        scope.launch {
+//                            snackbarHostState.showSnackbar("Hello")
+//                        }
+//                        Toast
+//                            .makeText(context, "TOAST", Toast.LENGTH_SHORT)
+//                            .show()
+//
+//                    },
+//
+//                tint = MaterialTheme.colors.primary,
+//            )
+//        }
         SnackbarHost(
             hostState = snackbarHostState,
             modifier = Modifier.align(Alignment.BottomEnd)
